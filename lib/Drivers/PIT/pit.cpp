46ef71b4711d66beae754fc18e3c7bf50a282bad
Ramon Papes <ramonpapes@gmail.com>
2024-08-09 21:34:55 -0300

    feat: PIT

---
+++ lib/Drivers/PIT/pit.cpp
@@ -0,0 +1,63 @@
+#include "pit.hpp"
+
+void Pit::init()
+{
+    gptimer_config_t gptimer_config = {
+        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+        .direction = GPTIMER_COUNT_UP,
+        .resolution_hz = 1000000,
+    };
+    ESP_ERROR_CHECK(gptimer_new_timer(&gptimer_config, &gptimer));
+    ESP_ERROR_CHECK(gptimer_enable(gptimer));
+}
+
+void Pit::start()
+{
+    ESP_ERROR_CHECK(gptimer_start(gptimer));
+}
+
+void Pit::stop()
+{
+    ESP_ERROR_CHECK(gptimer_stop(gptimer));
+}
+
+void Pit::write(uint64_t countsToPerform)
+{
+    uint64_t currentPitCount = get();
+
+    Timer.countLimit = currentPitCount + countsToPerform;
+
+    Timer.start_timer = currentPitCount;
+}
+
+void Pit::delay(uint64_t value)
+{
+    vTaskDelay(value / portTICK_PERIOD_MS);
+}
+
+void Pit::reset(){
+    ESP_ERROR_CHECK(gptimer_set_raw_count(gptimer, 0));
+}
+
+bool Pit::read()
+{
+    uint64_t currentPitCount;
+
+    currentPitCount = get();
+    if (currentPitCount >= Timer.CountLimit)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+uint64_t Pit::get()
+{
+    uint64_t currentPitCount;
+    ESP_ERROR_CHECK(gptimer_get_raw_count(gptimer, &currentPitCount));
+
+    return currentPitCount / 1000;
+}
---
+++ lib/Drivers/PIT/pit.hpp
@@ -0,0 +1,37 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gptimer.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    typedef struct
+    {
+        uint64_t start_timer;   
+        uint64_t countLimit;
+    } driverPITParammeters_t;
+    class Pit
+    {
+
+    public:
+        void init();
+        void start();
+        void stop();
+        void reset();
+        void write();
+        void delay();
+        bool read();
+        uint64_t get();
+
+    private:
+        gptimer_handle_t gptimer = NULL;
+        uint64_t readPITCounter();
+        driverPITParammeters_t Timer;
+    }
+
+#ifdef __cplusplus
+}
+#endif
