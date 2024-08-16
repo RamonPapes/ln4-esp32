0c53427b4f8e9f09582cc275e298fd13320ae163
PedroPortela-dev <pedrocaspop64@gmail.com>
2024-08-16 19:49:12 -0300

    feat: Create Servo Device

---
+++ lib/Devices/Servo/servo.hpp
@@ -0,0 +1,19 @@
+#ifndef SERVO_HPP
+#define SERVO_HPP
+
+class servo
+{
+    public:
+        servo();
+        ~servo();
+
+        void init();
+        void write();
+
+
+    private:
+
+};
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,24 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "GPIO/gpio.hpp"
+#include "PIT/pit.hpp"
 #include "PWM/pwm.hpp"
 
 
 extern "C" void app_main() 
 {
-    PWM pin(GPIO_NUM_2, LEDC_TIMER_8_BIT);
+    PWM pin(GPIO_NUM_2, LEDC_TIMER_16_BIT, 50);
 
     pin.init();
 
     while (1)
     {
-        for (uint8_t i = 0; i < 255; i = i<<1 | 1)
-        {
-            pin.write(i);
-            vTaskDelay(100/portTICK_PERIOD_MS);
-        }
-        for (uint8_t i = 255; i > 0; i = i>>1)
-        {
-            pin.write(i);
-            vTaskDelay(100/portTICK_PERIOD_MS);
-        }
-        
+        pin.write(6553);
+        Pit::delay(1000);
+        pin.write(4915);
+        Pit::delay(1000);
+        pin.write(3276);
+        Pit::delay(1000);
     }
     
 }
