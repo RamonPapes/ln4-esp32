5acfc3a440175bcd6b94cfb80e744f7128ca0e9c
PedroPortela-dev <pedrocaspop64@gmail.com>
2024-09-20 01:06:37 -0300

    teste Sensor Mux

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
-#include "PIT/pit.hpp"
-#include "Servo/servo.hpp"
-#include "Multiplexador/MUX_ADC.hpp"
+#include "devices.hpp"
+#include "drivers.hpp"
+#include "util.hpp"
 
+Gpio pin16(GPIO_NUM_16);
+Gpio pin17(GPIO_NUM_17);
+Gpio pin15(GPIO_NUM_15);
+Gpio pin2(GPIO_NUM_2);
+Gpio pin4(GPIO_NUM_4);
+
+Mux_GPIO sensorEsquerda(pin16, pin15, pin2, pin4);
+Mux_GPIO sensorDireita(pin17, pin15, pin2, pin4);
 
 extern "C" void app_main() 
 {
-    PWM pwm_servo(GPIO_NUM_2, LEDC_TIMER_16_BIT, 50);
-    servo motor_1(pwm_servo);
-
-    pwm_servo.init(20, 0);
-    motor_1.init(SERVO_CONTINUO);
+    sensorEsquerda.init();
+    sensorDireita.init();
 
     while (1)
     {
-        motor_1.front();
-        Pit::delay(1000);
-        motor_1.stop();
-        Pit::delay(1000);
-        motor_1.back();
-        Pit::delay(1000);
-        motor_1.stop();
-        Pit::delay(1000);
+        bool valor = sensorEsquerda.channel_0.read();
+        printlnf("VALOR: %d", valor);
+        Pit::delay(100);
     }
 }
