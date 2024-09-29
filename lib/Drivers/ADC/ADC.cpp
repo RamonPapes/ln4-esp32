94b184c046a596c331f0900a8cd5cf71f3d85a81
PedroPortela-dev <pedrocaspop64@gmail.com>
2024-09-29 17:59:29 -0300

    Remover setAttenuation e setWidth

--- lib/Drivers/ADC/ADC.cpp
+++ lib/Drivers/ADC/ADC.cpp
@@ -1,8 +1,10 @@
 #include "ADC.hpp";
 
 #define D_MAX 4085
-Adc::Adc(adc1_channel_t channel, adc_bits_width_t width, adc_atten_t atten)
-    : _channel(channel), _width(width), _atten(atten) {}
+#define MAX_VOLTS 2450
+
+Adc::Adc(adc1_channel_t channel)
+    : _channel(channel) {}
 
 void Adc::init()
 {
@@ -12,40 +14,10 @@ void Adc::init()
 
 int Adc::get()
 {
-    return  ESP_ERROR_CHECK(adc1_get_raw(_channel)); 
+    return  adc1_get_raw(_channel); 
 }
 
 int Adc::getVoltage()
 {
-    int maxVoltage;
-    if (_atten == ADC_ATTEN_DB_0)
-    {
-        maxVoltge = 950;
-    }
-    else if (_atten == ADC_ATTEN_DB_2_5)
-    {
-        maxVoltage = 1250;
-    }
-    else if (_atten == ADC_ATTEN_DB_6)
-    {
-        maxVoltage = 1750;
-    }
-    else if (atten == ADC_ATTEN_DB_11)
-    {
-        maxVoltage = 2450;
-    }
-
-    return this->get() * maxVoltage / D_MAX;
-}
-
-void ADC::setAttenuation(adc_atten_t atten)
-{
-    _atten = atten;
-    ESP_ERROR_CHECK(adc1_config_channel_atten(_channel, _atten));
-}
-
-void ADC::setWidth(adc_bits_width_t width)
-{
-    _width = width;
-    ESP_ERROR_CHECK(adc1_config_width(_width));
+    return this->get() * MAX_VOLTS / D_MAX;
 }
--- lib/Drivers/ADC/ADC.hpp
+++ lib/Drivers/ADC/ADC.hpp
@@ -1,27 +1,23 @@
-#include "freertos/FreeRTOS.h";
-#include "driver/adc.h";
+#ifndef ADC_HPP
+#define ADC_HPP
 
-#ifdef __cplusplus
-extern "C"
+#include "freertos/FreeRTOS.h"
+#include "driver/adc.h"
+
+class Adc
 {
-#endif
+protected:
+    Adc();
+public:
+    Adc(adc1_channel_t channel);
+    void init();
+    int getVoltage();
+    int get();
 
-    class Adc
-    {
-    public:
-        Adc(adc1_channel_t channel, adc_bits_width_t width = ADC_WIDTH_BIT_12, adc_atten_t atten = ADC_ATTEN_DB_11);
-        void setAttenuation(adc_atten_t atten);
-        void setWidth(adc_bits_width_t width);
-        void init();
-        int getVoltage();
-        int get();
+private:
+    adc1_channel_t _channel;
+    adc_atten_t _atten = ADC_ATTEN_DB_11;      // Atenuação
+    adc_bits_width_t _width = ADC_WIDTH_BIT_12; // Largura de bit
+};
 
-    private:
-        adc1_channel_t _channel;
-        adc_atten_t _atten;      // Atenuação
-        adc_bits_width_t _width; // Largura de bit
-    }
-
-#ifdef __cplusplus
-}
 #endif
