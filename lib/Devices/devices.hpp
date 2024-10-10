7a2c77ba76448f12fd5157530215bb13b64962a2
PedroPortela-dev <pedrocaspop64@gmail.com>
2024-10-09 21:24:14 -0300

    remover devices inuteis

--- lib/Devices/devices.hpp
+++ lib/Devices/devices.hpp
@@ -2,9 +2,6 @@
 #define DEVICES_HPP
 
 #include "DRV8833/DRV8833.hpp"
-#include "MUX_ADC/MUX_ADC.hpp"
-#include "Mux_GPIO/Mux_GPIO.hpp"
-#include "Servo/servo.hpp"
 #include "DRIVER_DRV8833/driver_drv8833.hpp"
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -26,27 +26,32 @@ Driver_Drv8833 driver(motor1, motor2);
 
 extern "C" void app_main() 
 {
-    sensor[0].init(-0.375, -0.5, 2040, 3770);
-    sensor[1].init(-0.25, -0.375, 2200, 3620);
-    sensor[2].init(-0.125, -0.25, 2390, 3630);
-    sensor[3].init(0, -0.125, 2340, 3430);
-    sensor[4].init(0, 0.125, 2040, 3120);
-    sensor[5].init(0.125, 0.25, 1980, 3160);
-    sensor[6].init(0.25, 0.375, 1990, 3790);
-    sensor[7].init(0.375, 0.5, 1980, 3590);
+    sensor[0].init(-0.75, -1, 2040, 3770);
+    sensor[1].init(-0.5, -0.75, 2200, 3620);
+    sensor[2].init(-0.25, -0.5, 2390, 3630);
+    sensor[3].init(0, -0.25, 2340, 3430);
+    sensor[4].init(0, 0.25, 2040, 3120);
+    sensor[5].init(0.25, 0.5, 1980, 3160);
+    sensor[6].init(0.5, 0.75, 1990, 3790);
+    sensor[7].init(0.75, 1, 1980, 3590);
 
     driver.init();
 
     driver.set_max_power(0.5);
 
     float direction;
+    float chegada;
 
     while (1)
     {
         direction = 0;
         for (size_t i = 0; i < 8; i++) direction += sensor[i].read();
         driver.arcade_driver(0.25+abs(direction)*0.75, direction);
+        for (int i = 0; i < 8; i++) chegada += abs(sensor[i].read());
+        if(chegada >= 1.8) break;
         printlnf("diferencial: %f", direction);
         vTaskDelay(1);
     }
+    driver.motor_driver(0, 0);
+    printlnf("CHEGOU!!!!!");
 }
